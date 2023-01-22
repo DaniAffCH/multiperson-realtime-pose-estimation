8b7accaa88e9a7f705fb378ed8bc07b35db9b897
DaniAffCH <danieleaffinita2000@gmail.com>
2023-01-22 20:18:41 +0100

    Added mobilenet blocks, sketched backbone and added backbone settings

--- src/lp_config/lp_model_config.py
+++ src/lp_config/lp_model_config.py
@@ -1,8 +1,46 @@
+from dataclasses import dataclass
+from typing import List
+
+@dataclass
+class MobileBlock_settings():
+    in_channels: int
+    out_channels: int
+    kernel_size: int
+    stride: int
+
+class Stage_settings():
+    def __init__(self, blocks: List[MobileBlock_settings]):
+        self.blocks = blocks
+        self.size = len(blocks)
+
+class Backbone_settings():
+    def __init__(self, stages: List[Stage_settings]):
+        self.stages = stages
+        self.size = len(stages)
 
 config = dict(
 
     litepose = dict(
-        largeKernels = 7
+        largeKernels = 7,
+        backbone = Backbone_settings([
+            # STAGE 1 
+            Stage_settings([
+                MobileBlock_settings(3,32,3,4),
+                MobileBlock_settings(5,6,7,8),
+                MobileBlock_settings(9,10,11,12)
+            ]),
+            # STAGE 2
+            Stage_settings([
+                MobileBlock_settings(3,32,3,4),
+                MobileBlock_settings(5,6,7,8),
+                MobileBlock_settings(9,10,11,12)
+            ]),
+            # STAGE 3
+            Stage_settings([
+                MobileBlock_settings(3,32,3,4),
+                MobileBlock_settings(5,6,7,8),
+                MobileBlock_settings(9,10,11,12)
+            ])
+        ])
     )
-
 )
--- src/lp_model/lp_common_layers.py
+++ src/lp_model/lp_common_layers.py
@@ -1,17 +1,36 @@
 import torch.nn as nn
 
 # it must be used always inside a Sequential()
-class convBlockBase(nn.Sequential):
-    def __init__(self, in_channels, out_channels, kernel_size, stride, activation=True):
+class ConvBlockBase(nn.Sequential):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, activation=True):
         if(activation):
-            super(convBlockBase, self).__init__(
+            super(ConvBlockBase, self).__init__(
                 nn.Conv2d(in_channels, out_channels, kernel_size, stride, kernel_size // 2, bias=False),
                 nn.BatchNorm2d(out_channels),
                 nn.ReLU6(inplace=True)
             )
         else:
-            super(convBlockBase, self).__init__(
+            super(ConvBlockBase, self).__init__(
                 nn.Conv2d(in_channels, out_channels, kernel_size, stride, kernel_size // 2, bias=False),
                 nn.BatchNorm2d(out_channels),
             )
 
+class ConvMobileBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1):
+        super(ConvMobileBlock, self).__init__()
+
+        self.useResidual = in_channels == out_channels and stride == 1
+
+        midChannels = in_channels+out_channels // 2
+        
+        self.s = nn.Sequential(
+            ConvBlockBase(in_channels, midChannels, 1, activation=False),
+            ConvBlockBase(midChannels, midChannels, kernel_size, stride, activation=False),
+            ConvBlockBase(midChannels, midChannels, 1)
+        )
+
+    def forward(self, x):
+        return self.s(x) + x if self.useResidual else self.s(x) 
+
+class ConvStage(nn.Module):
+    pass
